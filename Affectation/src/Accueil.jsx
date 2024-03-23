6cc42ed2ec05e2bcc68a5cd0ae11133127457ae8
MaloTSO <Malo.thomasson@free.fr>
2024-03-23 17:32:29 +0100

    Premier gros commit du site

---
+++ Affectation/src/Accueil.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import './Accueil.css'
+
+
+function Accueil() {
+  return (
+    <div className='accueil-container'>
+      <h1>Bienvenue sur le site de candidature aux projets</h1>
+      <p>
+        Sur ce site, vous pouvez candidater à une liste de projets proposés par des enseignants.
+        Classer les projets en fonction de vos préférences et soumettre votre candidature.
+      </p>
+      <Link to="/classementProjets">
+        <button>Classement des projets</button>
+      </Link>
+    </div>
+  );
+}
+
+export default Accueil;
---
+++ Affectation/src/ClassementProjets.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClassementProjets = () => {
+    return (
+        <div>
+            test
+        </div>
+    );
+};
+
+export default ClassementProjets;
