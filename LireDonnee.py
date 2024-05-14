eedcd89ae13d4dfb592b5b19e094985ab5ed8445
Rayan <perotti.rayan@gmail.com>
2024-05-14 22:57:29 +0200

    ajout fct recuperation donnée

--- LireDonnee.py
+++ LireDonnee.py
@@ -1,7 +1,51 @@
 import json 
 
-with open('test.json') as file:
-    data=json.load(file)
 
 
-print(data)
+def recupJSON(file):
+    with open(file) as f:
+        data=json.load(f)
+    return data
+
+
+data= recupJSON('bdd.json')
+
+def recupProjet(data):
+
+    Liste_Projet=[]
+    for i in range (len(data)):
+        tmp={}
+        tmp['Projet '+str(i)]=data[i]['nom']
+        tmp['Encadrant']=data[i]['responsable']
+        tmp['Groupes']=groupeProjet(data[i])
+        Liste_Projet.append(tmp)
+
+    return Liste_Projet
+
+def groupeProjet(projet):
+
+    liste_grp=[]
+
+    for g in projet['groupes']:
+        find= False
+        i=0
+        if len(liste_grp)==0:
+            tmp={}
+            tmp['Grp']=g['nom']
+            tmp['Score']=int(g['rang'])
+            liste_grp.append(tmp)
+        else:
+            while i< len(liste_grp):
+                if liste_grp[i]['Grp']==(g['nom']):
+                    liste_grp[i]['Score']+=int(g['rang'])
+                    find=True
+                i+=1
+            if not find:
+                tmp={}
+                tmp['Grp']=g['nom']
+                tmp['Score']=int(g['rang'])
+                liste_grp.append(tmp)
+    return liste_grp
+
+l=recupProjet(data)
+print(l)
